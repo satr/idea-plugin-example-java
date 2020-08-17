460e96c13caa3932573d9f918b6a1db5b93e0d23
GitHub Action <action@github.com>
2020-08-17 08:25:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/satr/ideapluginexamplejava/MyBundle.kt
+++ src/main/kotlin/com/github/satr/ideapluginexamplejava/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.satr.ideapluginexamplejava
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/satr/ideapluginexamplejava/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.satr.ideapluginexamplejava.services
+
+import com.github.satr.ideapluginexamplejava.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
